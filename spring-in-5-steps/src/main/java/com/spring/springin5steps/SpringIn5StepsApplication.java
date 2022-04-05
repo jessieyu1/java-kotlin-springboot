64f4955c9b6106c148f581707cd1faaac88f0779
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-05 12:56:47 +0800

    step 6
    troubleshooting:
    - beans / multi-beans/auto-wired

--- spring-in-5-steps/src/main/java/com/spring/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/spring/springin5steps/SpringIn5StepsApplication.java
@@ -12,7 +12,7 @@ public class SpringIn5StepsApplication {
 
 		//what are the beans?
 		//what are dependencies of a bean?
-		//where to search for beans?
+		// where to search for beans?
 
 //		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		//application context
