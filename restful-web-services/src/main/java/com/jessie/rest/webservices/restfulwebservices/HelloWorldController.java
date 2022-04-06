a9d90529ccccb833e268b82584137424bf87ee5d
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-06 11:00:26 +0800

    Get method
    -add path variable

--- restful-web-services/src/main/java/com/jessie/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/jessie/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.jessie.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
     @GetMapping(path = "hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+    //hello-world/path-variable/jessie
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
