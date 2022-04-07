2f99214c07cbcb13f3ad078dd6ebfdeb8b37b7dd
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-07 09:21:09 +0800

    add validation
    validation dependency

--- restful-web-services/src/main/java/com/jessie/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/jessie/rest/webservices/restfulwebservices/user/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
@@ -35,7 +36,7 @@ public class UserResource<T> {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
