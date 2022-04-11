e72da56d3c351bf54a56e431fe86d05c3350bb3c
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-11 14:44:38 +0800

    initial

---
+++ jap-in-10-steps/src/main/java/com/jessie/learning/jpa/japin10steps/JapIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.jessie.learning.jpa.japin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapIn10StepsApplication.class, args);
+	}
+
+}
---
+++ jap-in-10-steps/src/test/java/com/jessie/learning/jpa/japin10steps/JapIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessie.learning.jpa.japin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
