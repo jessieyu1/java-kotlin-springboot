227ecbe72946abbd026f68946bfc10e2683c8f06
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-12 11:14:27 +0800

    findbyid and findbyall

--- jap-in-10-steps/src/main/java/com/jessie/learning/jpa/japin10steps/UserRepositoryCommandLineRunner.java
+++ jap-in-10-steps/src/main/java/com/jessie/learning/jpa/japin10steps/UserRepositoryCommandLineRunner.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -21,6 +24,13 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
         User user = new User("Jill", "Admin");
         userRepository.save(user);
         log.info("New User is created : " + user);
+
+        Optional<User> userWithIdOne = userRepository.findById(1L);
+        log.info("User is retrived : " + userWithIdOne);
+
+        List<User> users = userRepository.findAll();
+        log.info("All Users : " + users);
+
 
     }
 }
